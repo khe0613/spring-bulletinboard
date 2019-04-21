5848d64ebdbde49a8f318e128676df145362fefa
khe0613 <khe0616@cnu.ac.kr>
2019-04-21 22:43:20 +0900

    MySQL 연동(application.yml) + Member Repository Test(회원 저장/불러오기)

---
+++ src/test/java/com/khe0613/springbulletinboard/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.khe0613.springbulletinboard.domain.member;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @After
+    public void cleanup(){
+        /*
+        이후 테스트 코드에 영향을 끼치지 않기 위해
+        테스트 메소드가 끝날때 마다 repository 전체를 비우는 코드
+         */
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    public void 회원저장_불러오기(){
+        //given
+        memberRepository.save(Member.builder()
+                    .id("khe0616")
+                    .password("changheekim")
+                    .name("홍길동")
+                    .tel("010-1111-1111")
+                    .build());
+
+        //when
+        List<Member> memberList = memberRepository.findAll();
+
+        //then
+        Member member = memberList.get(0);
+        assertThat(member.getId(), is("khe0616"));
+        assertThat(member.getPassword(), is("changheekim"));
+        assertThat(member.getName(), is("홍길동"));
+        assertThat(member.getTel(), is("010-1111-1111"));
+    }
+
+}
