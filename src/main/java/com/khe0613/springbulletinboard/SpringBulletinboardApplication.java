ccf2964bfe312f48859475c65f28dcedcae214c6
khe0613 <khe0616@cnu.ac.kr>
2019-04-21 19:01:20 +0900

    Create Project!

---
+++ src/main/java/com/khe0613/springbulletinboard/SpringBulletinboardApplication.java
@@ -0,0 +1,13 @@
+package com.khe0613.springbulletinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBulletinboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBulletinboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khe0613/springbulletinboard/SpringBulletinboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khe0613.springbulletinboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBulletinboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
