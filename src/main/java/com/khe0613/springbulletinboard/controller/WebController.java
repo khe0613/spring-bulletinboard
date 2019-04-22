c6dbc4eeb645f2cca834843976a7c34df0202153
khe0613 <khe0616@cnu.ac.kr>
2019-04-22 14:07:26 +0900

    Rename package : com.khe0613.springbulletinboard.web -> com.khe0613.springbulletinboard.controller

--- src/main/java/com/khe0613/springbulletinboard/controller/WebController.java
+++ src/main/java/com/khe0613/springbulletinboard/controller/WebController.java
@@ -1,4 +1,4 @@
-package com.khe0613.springbulletinboard.web;
+package com.khe0613.springbulletinboard.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/com/khe0613/springbulletinboard/controller/WebControllerTest.java
+++ src/test/java/com/khe0613/springbulletinboard/controller/WebControllerTest.java
@@ -1,4 +1,4 @@
-package com.khe0613.springbulletinboard.web;
+package com.khe0613.springbulletinboard.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
