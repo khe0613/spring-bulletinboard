b1e744c2ef8d332bad051eb376155e2548dda680
khe0613 <khe0616@cnu.ac.kr>
2019-04-27 18:04:13 +0900

    공통적인 Navbar에 대해 Fragment 사용

--- src/main/resources/static/js/app/main.js
+++ src/main/resources/static/js/app/main.js
@@ -1,12 +1,23 @@
 var main = {
     init: function(){
         var _this = this;
-        $("#btn-signup").on('click', function(){
+        $("#btn-signup").on('click', function(){    // 회원가입 버튼 클릭
            _this.signup();
+            event.preventDefault();         // 이벤트 전파 방지
+        });
+
+        $('#btn-login').on('click', function(){     // 로그인 버튼 클릭
+            _this.login();
+            event.preventDefault();         // 이벤트 전파 방지
         });
     },
+
     signup: function(){
         location.href = "/signup";
+    },
+
+    login: function(){
+
     }
 };
 
