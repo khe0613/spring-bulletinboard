9c4bae8900bcc3a2f873edc841203d4ce47c9ef2
khe0613 <khe0616@cnu.ac.kr>
2019-04-28 21:30:43 +0900

    게시판 기능 테스트 쿼리 추가

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,2 +1,4 @@
 insert into members(id, password, name, tel) values ('aaa123', 'pwdaaa', '홍길동', '010-4444-9999');
 insert into members(id, password, name, tel) values ('bbb123', 'pwdbbb', '공유', '010-3333-1111');
+insert into posts(post_number, title, content, writer_number) values(1, '공유의 게시물', '안녕하세요 공유입니다.', 2);
+insert into posts(post_number, title, content, writer_number) values(2, '홍길동 게시물', '안녕하세요 홍길동입니다.', 1);
