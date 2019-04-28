c2b05c9471826798d6c3889b6839d84afb88221a
khe0613 <khe0616@cnu.ac.kr>
2019-04-28 13:34:32 +0900

    button에 대한 click 이벤트 callback function의 parameter에 event 추가

--- src/main/resources/static/js/app/common-navbar.js
+++ src/main/resources/static/js/app/common-navbar.js
@@ -1,14 +1,23 @@
 var main = {
     init: function(){
         var _this = this;
-        $('#btn-logout').on('click', function () {
+        $('#btn-logout').on('click', function (event) {
            _this.logout();
            event.preventDefault();     // 이벤트 전파 방지
+        });
+
+        $('#btn-mypage').on('click', function (event) {
+            _this.getMyPage();
+            event.preventDefault()
         });
     },
 
     logout: function(){
         location.href='/logout';
+    },
+    
+    getMyPage: function () {
+        //location.href = '//';
     }
 };
 
--- src/main/resources/static/js/app/main.js
+++ src/main/resources/static/js/app/main.js
@@ -1,12 +1,12 @@
 var main = {
     init: function(){
         var _this = this;
-        $("#btn-signup").on('click', function(){    // 회원가입 버튼 클릭
+        $("#btn-signup").on('click', function(event){    // 회원가입 버튼 클릭
            _this.signup();
             event.preventDefault();         // 이벤트 전파 방지
         });
 
-        $('#btn-login').on('click', function(){     // 로그인 버튼 클릭
+        $('#btn-login').on('click', function(event){     // 로그인 버튼 클릭
             _this.login();
             event.preventDefault();         // 이벤트 전파 방지
         });
