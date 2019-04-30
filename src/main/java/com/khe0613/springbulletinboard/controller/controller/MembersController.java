427f2e37f104e7b2e6af2d31ad7acb8cc8488210
khe0613 <khe0616@cnu.ac.kr>
2019-04-30 16:09:28 +0900

    마이페이지에 대한 에러 처리 추가

--- src/main/java/com/khe0613/springbulletinboard/controller/controller/MembersController.java
+++ src/main/java/com/khe0613/springbulletinboard/controller/controller/MembersController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 
@@ -18,34 +20,55 @@ public class MembersController {
 
     // 회원정보 조회(마이페이지)
     @GetMapping("/members")
-    public String getMember(HttpSession session){
+    public ModelAndView getMember(HttpSession session, ModelAndView modelAndView, RedirectAttributes redirectAttributes){
         System.out.println("GET members");
 
-        String id = session.getAttribute("userId").toString();
+        // 로그인 안되있는 경우
+        if(session.getAttribute("userId") == null){
+            String login_message = "로그인이 필요한 서비스입니다.";
 
+            modelAndView.setViewName("redirect:/");
+            redirectAttributes.addFlashAttribute("login_message", login_message);
+            return modelAndView;
+        }
+
+        // 로그인 되어 있는 경우,
         // 현재 로그인되어 있는 계정의 정보를 조회
-        return "redirect:members/" + id;
+        modelAndView.setViewName("redirect:members/" + session.getAttribute("userId").toString());
+        return modelAndView;
     }
 
     // 회원정보 조회(마이페이지)
     @GetMapping("/members/{id}")
-    public String getMember(@PathVariable("id") String id, Model model, HttpSession session) {
+    public ModelAndView getMember(@PathVariable("id") String id, HttpSession session, ModelAndView modelAndView, RedirectAttributes redirectAttributes) {
         System.out.println("GET members/{id}");
+
+        // 로그인 안되있는 경우
+        if(session.getAttribute("userId") == null){
+            String login_message = "로그인이 필요한 서비스입니다.";
+
+            modelAndView.setViewName("redirect:/");
+            redirectAttributes.addFlashAttribute("login_message", login_message);
+            return modelAndView;
+        }
+
         String loginId = session.getAttribute("userId").toString();
-
-        // 로그인 되어 있지 않은 경우(loginId가 null)에 대한 처리 필요함
-
 
         // 현재 로그인된 회원이 다른 회원의 마이페이지 조회를 요청할 경우
         // 요청이 그대로 수행되지 않고, 현재 로그인된 회원의 정보가 조회됨
         // id -> Request된 id
         // loginId -> 실제로 로그인 되어 있는 id
         if(!id.equals(loginId)){
-            return "redirect:/members/" + loginId;
+            modelAndView.setViewName("redirect:/members/" + loginId);
+            return modelAndView;
         }
 
         Members member = membersService.getMember(id);
-        model.addAttribute("userInfo", member);
-        return "mypage";
+        modelAndView.setViewName("mypage");
+        modelAndView.addObject("userInfo", member);
+        modelAndView.addObject("user_login", true);
+        modelAndView.addObject("userId", loginId);
+
+        return modelAndView;
     }
 }
